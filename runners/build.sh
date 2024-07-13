85fa5c72f1fcb562784865613f3ad25b258359a4
cyciling <1377899390@qq.com>
2024-07-14 02:40:26 +0800

    Update build.sh

--- runners/build.sh
+++ runners/build.sh
@@ -3,11 +3,11 @@
 [ -d "${GITHUB_WORKSPACE}" ] && PROJECT_DIR="${GITHUB_WORKSPACE}"
 [ "${PROJECT_DIR}" != "${PWD}" ] && cd "${PROJECT_DIR}"
 
-mkdir artifacts
-mkdir artifacts/overrides
-for dir in $(ls )
+mkdir .artifacts
+mkdir .artifacts/overrides
+for dir in $(ls)
     do
-        mv $dir artifacts/overrides
+        mv $dir .artifacts/overrides
     done 
-mv manifest.json artifacts
-mv modlist.html artifacts
+mv manifest.json .artifacts
+mv modlist.html .artifacts
