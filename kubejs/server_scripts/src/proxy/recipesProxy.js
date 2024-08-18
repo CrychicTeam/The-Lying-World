28e4bbf6211686ed1482a38335db0440f51f7ef1
cyciling <1377899390@qq.com>
2024-08-18 13:28:08 +0800

    update

--- kubejs/server_scripts/src/proxy/recipesProxy.js
+++ kubejs/server_scripts/src/proxy/recipesProxy.js
@@ -1,7 +1,7 @@
 // priority: -1
-const { CraftingTableCobblefordays } = require("../recipes/cobblefordays/craftingTable")
-const { removeCobblefordays } = require("../recipes/cobblefordays/remove")
-const { CraftingTableQuark } = require("../recipes/quark/craftingTable")
+// const { CraftingTableCobblefordays } = require("../recipes/cobblefordays/craftingTable")
+// const { removeCobblefordays } = require("../recipes/cobblefordays/remove")
+// const { CraftingTableQuark } = require("../recipes/quark/craftingTable")
 
 ServerEvents.recipes(event => {
     removeCobblefordays(event)
